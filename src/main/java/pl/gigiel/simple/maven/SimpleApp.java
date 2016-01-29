e5d19140bcdd946d43bfc37f917bd05c5f296465
Tomasz Gigiel <tomasz.gigiel@gmail.com>
2016-01-29 21:27:24 +0100

    start

---
+++ src/main/java/pl/gigiel/simple/maven/SimpleApp.java
@@ -0,0 +1,7 @@
+package pl.gigiel.simple.maven;
+
+public class SimpleApp {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ src/test/java/pl/gigiel/simple/maven/SimpleAppTest.java
@@ -0,0 +1,11 @@
+package pl.gigiel.simple.maven;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleAppTest {
+	@Test
+	public void something() {
+		Assert.assertEquals(1, 1);
+	}
+}
